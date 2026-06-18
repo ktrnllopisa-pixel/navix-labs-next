eb86041eb609c0d019424344ada566dbeefa5908
ktrnllopisa-pixel <ktrnllopis.a@gmail.com>
2026-06-18 18:20:54 -0400

    Update industries.js

--- data/industries.js
+++ data/industries.js
@@ -1,1 +1,10 @@
-
+export const industries = [
+  "Distribuidoras de agua",
+  "Reparto de gas",
+  "Ferreterías y materiales",
+  "Fletes urbanos",
+  "Empresas de técnicos en terreno",
+  "Delivery local",
+  "Servicios B2B",
+  "Operaciones con múltiples direcciones",
+];
