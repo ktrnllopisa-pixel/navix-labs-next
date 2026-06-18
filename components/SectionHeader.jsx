50eef03986ff089019d20e899541a814dbcda239
ktrnllopisa-pixel <ktrnllopis.a@gmail.com>
2026-06-18 18:17:37 -0400

    Update SectionHeader.jsx

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -1,1 +1,9 @@
-
+export default function SectionHeader({ eyebrow, title, text }) {
+  return (
+    <div className="sectionHeader">
+      {eyebrow && <p className="eyebrow">{eyebrow}</p>}
+      <h2>{title}</h2>
+      {text && <p>{text}</p>}
+    </div>
+  );
+}
