1247a6024379a915dd6b818048df4ddd7659c856
ktrnllopisa-pixel <ktrnllopis.a@gmail.com>
2026-06-18 18:18:55 -0400

    Update ProcessStep.jsx

--- components/ProcessStep.jsx
+++ components/ProcessStep.jsx
@@ -1,1 +1,11 @@
-
+export default function ProcessStep({ number, title, text }) {
+  return (
+    <article className="processStep">
+      <span>{number}</span>
+      <div>
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </div>
+    </article>
+  );
+}
