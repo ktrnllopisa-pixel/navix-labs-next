5d7b6c8221385cb74aa2af0697aab1176d791189
ktrnllopisa-pixel <ktrnllopis.a@gmail.com>
2026-06-18 18:18:25 -0400

    Update ServiceCard.jsx

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -1,1 +1,16 @@
+export default function ServiceCard({ title, description, points = [] }) {
+  return (
+    <article className="serviceCard">
+      <h3>{title}</h3>
+      <p>{description}</p>
 
+      {points.length > 0 && (
+        <ul>
+          {points.map((point) => (
+            <li key={point}>{point}</li>
+          ))}
+        </ul>
+      )}
+    </article>
+  );
+}
