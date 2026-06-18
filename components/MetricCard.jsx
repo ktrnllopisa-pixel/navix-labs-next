bfbd84f009a4cd545d6c1d397f3232f5941b3c0d
ktrnllopisa-pixel <ktrnllopis.a@gmail.com>
2026-06-18 18:18:00 -0400

    Update MetricCard.jsx

--- components/MetricCard.jsx
+++ components/MetricCard.jsx
@@ -1,1 +1,9 @@
-
+export default function MetricCard({ value, label, detail }) {
+  return (
+    <article className="metricCard">
+      <strong>{value}</strong>
+      <span>{label}</span>
+      {detail && <p>{detail}</p>}
+    </article>
+  );
+}
